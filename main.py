ed99ae5fa88d934250bb9157bbd021fe549af631
KTrain5369 <69028025+KTrain5169@users.noreply.github.com>
2024-08-30 11:26:39 +1000

    bugfixing

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ def output_converted(case, text):
         converted_text = text.lower()
     else:
         print("Something went wrong.")
-        error = 1
     print(f"The result is: {converted_text}")
     pyperclip.copy(converted_text)
     with open('output.txt', 'a') as f:
@@ -23,24 +22,25 @@ def output_converted(case, text):
     print("I added the converted text into a file called output.txt, if it's easier to copy from there.")
 
 def prompt_redo():
-    if error == 1: 
-        redo = str(input("Restart? (y/n) "))
-    else:
-        redo = str(input("Continue? (y/n) "))
+    redo = str(input("Continue? (y/n) "))
     if redo == "y":
-        i += 1
-        error = 0
+        return "redo"
     elif redo == "n":
-        break
+        return "stop"
+
 
 print("At any time, type EXIT to stop. ")
 while i < 1:
+    err = 0
     mode = int(input("Type 1 for uppercase, 2 for lowercase. "))
     text_input = str(input("What would you like to fully uppercase/lowercase? "))
-
     if mode == 1:
         output_converted(mode, text_input)
-        prompt_redo()
+        restart = prompt_redo()
+        if restart == "stop":
+            break
     elif mode == 2:
         output_converted(mode, text_input)
-        prompt_redo()
+        restart = prompt_redo()
+        if restart == "stop":
+            break
