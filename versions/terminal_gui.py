b0735d7608d211ee033aa877f780fe05dc79ce09
KTrain5369 <69028025+KTrain5169@users.noreply.github.com>
2024-09-12 19:25:10 +1000

    remove unneeded imports in terminal_gui

--- versions/terminal_gui.py
+++ versions/terminal_gui.py
@@ -3,8 +3,6 @@ import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from library.TextConverter import TextConverter
-from PIL import Image
-import io
 
 
 def clear_screen():
