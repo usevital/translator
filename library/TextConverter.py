354990d45c2f5379fc5d9b69d13f2903cfa06c29
Thinkseal <132022649+Thinkseal@users.noreply.github.com>
2024-10-19 14:01:09 -0400

    Improved flipped translation for U in Minecraft

--- library/TextConverter.py
+++ library/TextConverter.py
@@ -54,7 +54,7 @@ class TextConverter:
     def flipUD(self, text):
         flip_map = str.maketrans(
             "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890.,!?/\"'()[]{\}",
-            "ɐqɔpǝɟɓɥᴉſʞlɯuodbɹsʇnʌʍxʎzⱯᗺƆᗡƎℲ⅁HIſꓘꞀWNOԀꝹᴚS⟘ꓵΛMX⅄Z⇂ᘕԐત૨୧L8მ0·'¡¿\„,)(][}/{"
+            "ɐqɔpǝɟɓɥᴉſʞlɯuodbɹsʇnʌʍxʎzⱯᗺƆᗡƎℲ⅁HIſꓘꞀWNOԀꝹᴚS⟘∩ΛMX⅄Z⇂ᘕԐત૨୧L8მ0·'¡¿\„,)(][}/{"
         )
         return text.translate(flip_map)
 
