3f5a158c15abab74f8a830f9e16b45aa2c641490
Thinkseal <132022649+Thinkseal@users.noreply.github.com>
2024-10-19 13:15:57 -0400

    improved upside down translation for A in Minecraft

--- library/TextConverter.py
+++ library/TextConverter.py
@@ -54,7 +54,7 @@ class TextConverter:
     def flipUD(self, text):
         flip_map = str.maketrans(
             "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890.,!?/\"'()[]{\}",
-            "ɐqɔpǝɟɓɥᴉſʞlɯuodbɹsʇnʌʍxʎz∀ꓭƆꓷƎℲꓨHIſꓘ⅃WNOꓒΌꓤSꓕꓵΛMX⅄Z⇂ᘕԐત૨୧L8მ0·ˋ¡¿\„,)(][}/{"
+            "ɐqɔpǝɟɓɥᴉſʞlɯuodbɹsʇnʌʍxʎzⱯꓭƆꓷƎℲꓨHIſꓘ⅃WNOꓒΌꓤSꓕꓵΛMX⅄Z⇂ᘕԐત૨୧L8მ0·ˋ¡¿\„,)(][}/{"
         )
         return text.translate(flip_map)
 
