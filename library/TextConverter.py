b0d96485f7dea26a240cec70987b90393b6a7d5a
Thinkseal <132022649+Thinkseal@users.noreply.github.com>
2024-10-19 13:46:22 -0400

    Improved flipped translation for P in Minecraft

--- library/TextConverter.py
+++ library/TextConverter.py
@@ -54,7 +54,7 @@ class TextConverter:
     def flipUD(self, text):
         flip_map = str.maketrans(
             "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890.,!?/\"'()[]{\}",
-            "ɐqɔpǝɟɓɥᴉſʞlɯuodbɹsʇnʌʍxʎzⱯᗺƆᗡƎℲ⅁HIſꓘꞀWNOꓒΌꓤS⟘ꓵΛMX⅄Z⇂ᘕԐત૨୧L8მ0·'¡¿\„,)(][}/{"
+            "ɐqɔpǝɟɓɥᴉſʞlɯuodbɹsʇnʌʍxʎzⱯᗺƆᗡƎℲ⅁HIſꓘꞀWNOԀΌꓤS⟘ꓵΛMX⅄Z⇂ᘕԐત૨୧L8მ0·'¡¿\„,)(][}/{"
         )
         return text.translate(flip_map)
 
