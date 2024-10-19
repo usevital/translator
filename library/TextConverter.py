a45173502cabd1767d80187348f975caf2f4b458
Thinkseal <132022649+Thinkseal@users.noreply.github.com>
2024-10-19 13:27:20 -0400

    Improved fliped translation for L

--- library/TextConverter.py
+++ library/TextConverter.py
@@ -54,7 +54,7 @@ class TextConverter:
     def flipUD(self, text):
         flip_map = str.maketrans(
             "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890.,!?/\"'()[]{\}",
-            "ɐqɔpǝɟɓɥᴉſʞlɯuodbɹsʇnʌʍxʎzⱯᗺƆꓷƎℲ⅁HIſꓘ⅃WNOꓒΌꓤS⟘ꓵΛMX⅄Z⇂ᘕԐત૨୧L8მ0·ˋ¡¿\„,)(][}/{"
+            "ɐqɔpǝɟɓɥᴉſʞlɯuodbɹsʇnʌʍxʎzⱯᗺƆꓷƎℲ⅁HIſꓘꞀWNOꓒΌꓤS⟘ꓵΛMX⅄Z⇂ᘕԐત૨୧L8მ0·ˋ¡¿\„,)(][}/{"
         )
         return text.translate(flip_map)
 
