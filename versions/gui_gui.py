7ac7ed4de266a6068d67bb0f140b59e8826b321c
Thinkseal <132022649+Thinkseal@users.noreply.github.com>
2024-10-27 13:32:06 -0400

    Update gui_gui.py

--- versions/gui_gui.py
+++ versions/gui_gui.py
@@ -9,22 +9,25 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from library.TextConverter import TextConverter
 converter = TextConverter()
 
-def copy_to_clipboard(text):
-    pyperclip.copy(text.get())
+def FlipedTextCopy(text):
+    pyperclip.copy(converter.reverse_text(converter.text_flip(text.get())))
 
 def add_text(text_input):
-    ttk.Label( text=converter.reverse_text(text_input.get())).grid(row=2,column=2)
-    ttk.Label( text=converter.text_flip(text_input.get())).grid(row=4,column=2)
-    ttk.Label( text=converter.enchant_text(text_input.get())).grid(row=5,column=2)
+    ttk.Label( text=converter.reverse_text(text_input.get()),justify="left").grid(sticky = W,row=2,column=2)
+    ttk.Label( text=converter.text_flip(text_input.get()),justify="left").grid(sticky = W,row=4,column=2)
+    ttk.Label( text=converter.enchant_text(text_input.get()),justify="left").grid(sticky = W,row=5,column=2)
 
 def add_copy_buttons():
-    ttk.Button(root, text="copy", command=partial(copy_to_clipboard, translate_text),).grid(row=2, column=1)
+    ttk.Button(root, command=partial(FlipedTextCopy, translate_text), image = copy_icon).grid(row=2, column=1)
 
 root = Tk()
 root.title("Translator")
 ico = Image.open('images/icon.ico')
 photo = ImageTk.PhotoImage(ico)
 root.wm_iconphoto(False, photo)
+root.geometry("600x300")
+copy_icon = Image.open('images/content_copy.png')
+copy_icon = ImageTk.PhotoImage(copy_icon)
 
 ttk.Label( text="Enter text to translate:").grid(column=0, row=0)
 translate_text = Entry(root)
