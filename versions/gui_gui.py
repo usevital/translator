f05d8ce0c279626f400924bd7b31b7a18e6d175f
Thinkseal <132022649+Thinkseal@users.noreply.github.com>
2024-11-13 18:10:00 -0500

    Update gui_gui.py

--- versions/gui_gui.py
+++ versions/gui_gui.py
@@ -8,36 +8,93 @@ from PIL import Image, ImageTk
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from library.TextConverter import TextConverter
 converter = TextConverter()
+text_lable_column=0
+copy_button_column=1
+text_output_column=2
 
-def FlipedTextCopy(text):
-    pyperclip.copy(converter.reverse_text(converter.text_flip(text.get())))
+def copy_to_clipboard(text):
+    pyperclip.copy(text['text'])
 
-def add_text(text_input):
-    x_size=int(9*len(text_input.get())+300)
-    root.geometry(f"{x_size}x{300}")
-    ttk.Label( text=converter.reverse_text(text_input.get()),justify="left").grid(sticky = W,row=2,column=2)
-    ttk.Label( text=converter.text_flip(text_input.get()),justify="left").grid(sticky = W,row=4,column=2)
-    ttk.Label( text=converter.enchant_text(text_input.get()),justify="left").grid(sticky = W,row=5,column=2)
+def add_text(*args):
+    x_size=int(8.5*len(translate_text.get())+240)
+    root.geometry(f"{x_size}x{380}")
+    new_text = translate_text.get()
+    reversed_text['text'] = converter.reverse_text(new_text)
+    flipped_text['text'] = converter.text_flip(new_text)
+    enchanted_text['text'] = converter.enchant_text(new_text)
+    capitalized_text['text'] = converter.case_switch(new_text, "upper")
+    lowercased_text['text'] = converter.case_switch(new_text, "lower")
+    leetspeak_text['text'] = converter.leetspeak(new_text)
+    scrambled_text['text'] = converter.scramble_text(new_text)
+    piglatin_text['text'] = converter.piglatin(new_text)
+    zalgo_text['text'] = converter.zalgo_text(new_text)
+    emoticons_text['text'] = converter.text_to_emoticons(new_text)
 
 def add_copy_buttons():
-    ttk.Button(root, command=partial(FlipedTextCopy, translate_text), image = copy_icon).grid(row=2, column=1)
+    ttk.Button(root, command=partial(copy_to_clipboard, reversed_text), image = copy_icon).grid(sticky = W, row=1, column=copy_button_column)
+    ttk.Button(root, command=partial(copy_to_clipboard, flipped_text), image = copy_icon).grid(sticky = W, row=2, column=copy_button_column)
+    ttk.Button(root, command=partial(copy_to_clipboard, enchanted_text), image = copy_icon).grid(sticky = W, row=3, column=copy_button_column)
+    ttk.Button(root, command=partial(copy_to_clipboard, capitalized_text), image = copy_icon).grid(sticky = W, row=4, column=copy_button_column)
+    ttk.Button(root, command=partial(copy_to_clipboard, capitalized_text), image = copy_icon).grid(sticky = W, row=5, column=copy_button_column)
+    ttk.Button(root, command=partial(copy_to_clipboard, leetspeak_text), image = copy_icon).grid(sticky = W, row=6, column=copy_button_column)
+    ttk.Button(root, command=partial(copy_to_clipboard, scrambled_text), image = copy_icon).grid(sticky = W, row=7, column=copy_button_column)
+    ttk.Button(root, command=partial(copy_to_clipboard, piglatin_text), image = copy_icon).grid(sticky = W, row=8, column=copy_button_column)
+    ttk.Button(root, command=partial(copy_to_clipboard, zalgo_text), image = copy_icon).grid(sticky = W, row=9, column=copy_button_column)
+    ttk.Button(root, command=partial(copy_to_clipboard, emoticons_text), image = copy_icon).grid(sticky = W, row=10, column=copy_button_column)
 
 root = Tk()
 root.title("Translator")
 ico = Image.open('images/icon.ico')
 photo = ImageTk.PhotoImage(ico)
 root.wm_iconphoto(False, photo)
-root.geometry("300x300")
+root.geometry("240x380")
 copy_icon = Image.open('images/content_copy.png')
 copy_icon = ImageTk.PhotoImage(copy_icon)
 
-ttk.Label( text="Enter text to translate:").grid(column=0, row=0)
-translate_text = Entry(root)
-translate_text.grid(row=1, column=0)
-ttk.Label( text="Reversed text:").grid(row=2,column=0)
-ttk.Label( text="Fliped text:").grid(row=4,column=0)
-ttk.Label( text="Enchanted text:").grid(row=5,column=0)
-ttk.Button(root, text="Submit", command=partial(add_text, translate_text),).grid(row=1, column=1)
-add_copy_buttons()
+translate_text = StringVar()
+translate_text.trace_add("write", add_text)
+input_widget = ttk.Entry(root, textvariable=translate_text)
+input_widget.grid(row=0, column=0, columnspan=2)
 
+ttk.Label(text="Reversed text:").grid(row=1,column=text_lable_column)
+reversed_text = ttk.Label(justify="left")
+reversed_text.grid(row=1, column=text_output_column, sticky = W)
+
+ttk.Label(text="Flipped text:").grid(row=2,column=text_lable_column)
+flipped_text = ttk.Label(justify="left")
+flipped_text.grid(row=2, column=text_output_column, sticky = W)
+
+ttk.Label(text="Enchanted text:").grid(row=3,column=text_lable_column)
+enchanted_text = ttk.Label(justify="left")
+enchanted_text.grid(row=3, column=text_output_column, sticky = W)
+
+ttk.Label(text="Capitalized text:").grid(row=4,column=text_lable_column)
+capitalized_text = ttk.Label(justify="left")
+capitalized_text.grid(row=4, column=text_output_column, sticky = W)
+
+ttk.Label(text="Lowercased text:").grid(row=5,column=text_lable_column)
+lowercased_text = ttk.Label(justify="left")
+lowercased_text.grid(row=5, column=text_output_column, sticky = W)
+
+ttk.Label(text="Leetspeak text:").grid(row=6,column=text_lable_column)
+leetspeak_text = ttk.Label(justify="left")
+leetspeak_text.grid(row=6, column=text_output_column, sticky = W)
+
+ttk.Label(text="Scrambled text:").grid(row=7,column=text_lable_column)
+scrambled_text = ttk.Label(justify="left")
+scrambled_text.grid(row=7, column=text_output_column, sticky = W)
+
+ttk.Label(text="piglatin text:").grid(row=8,column=text_lable_column)
+piglatin_text = ttk.Label(justify="left")
+piglatin_text.grid(row=8, column=text_output_column, sticky = W)
+
+ttk.Label(text="zalgo text:").grid(row=9,column=text_lable_column)
+zalgo_text = ttk.Label(justify="left")
+zalgo_text.grid(row=9, column=text_output_column, sticky = W)
+
+ttk.Label(text="emoticons text:").grid(row=10,column=text_lable_column)
+emoticons_text = ttk.Label(justify="left")
+emoticons_text.grid(row=10, column=text_output_column, sticky = W)
+
+add_copy_buttons()
 root.mainloop()
